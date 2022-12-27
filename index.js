c286821129f3bb90fad40a7932cb3c1308a5d05e
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-26 23:43:15 -0500

    renderLibrary function

--- index.js
+++ index.js
@@ -7,10 +7,34 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
-
 }
 
 function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
+}
+
+function renderLibrary(library) {
+    let table = document.querySelector('#lib-tab');
+    library.forEach(book => {
+        let row = document.createElement('tr');
+
+        let title = document.createElement('td');
+        title.textContent = book.title;
+        row.appendChild(title);
+
+        let author = document.createElement('td');
+        author.textContent = book.author;
+        row.appendChild(author);
+
+        let pages = document.createElement('td');
+        pages.textContent = book.pages;
+        row.appendChild(pages);
+
+        let read = document.createElement('td');
+        read.textContent = book.read;
+        row.appendChild(pages);
+
+        table.appendChild(row);
+    });
 }
